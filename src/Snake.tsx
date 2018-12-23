73c9adb3fc8ff0acf505c6930e36a6e77ef4e2ee
arzamax <arzamax@gmail.com>
2018-12-23 15:16:18 +0400

    added collision logic

--- src/Snake.tsx
+++ src/Snake.tsx
@@ -23,34 +23,111 @@ interface ISnakeState {
   snakeCoordinates: Array<ICell>,
   direction: Direction,
   foodCoordinates: ICell,
+  score: number,
 }
 
 const GRAY_COLOR = '#949494';
 const WHITE_COLOR = '#ffffff';
 const FIELD_LENGTH = 20;
+const INITIAL_SNAKE_SIZE = 5;
+
+/**
+ * @function getRandomCoordinate
+ * @param {number} cellSize
+ * @returns ICell
+ * @description Returns random coordinate.
+ */
 
 const getRandomCoordinate = (cellSize: number): number => Math.floor(Math.random() * FIELD_LENGTH) * cellSize;
 
-const isSameCoordinates = (a: ICell, b: ICell) => a.x === b.x && a.y === b.y;
+/**
+ * @function isSameCoordinates
+ * @param {ICell} a
+ * @param {ICell} b
+ * @returns {boolean}
+ * @description Compares two coordinates.
+ */
+
+const isSameCoordinates = (a: ICell, b: ICell): boolean => a.x === b.x && a.y === b.y;
+
+/**
+ *
+ * @param {ICell} headCoordinates
+ * @param {Direction} direction
+ * @param {number} canvasSize
+ * @param {number} cellSize
+ * @returns {ICell | null}
+ * @description Computes snake's head next coordinate.
+ */
+
+const getComputedNextHeadCoordinates = (
+  headCoordinates: ICell,
+  direction: Direction,
+  canvasSize: number,
+  cellSize: number
+): ICell | null => {
+  if (headCoordinates) {
+    let { x, y } = headCoordinates;
+
+    switch (direction) {
+      case Direction.Right:
+        x += cellSize;
+        break;
+      case Direction.Left:
+        x -= cellSize;
+        break;
+      case Direction.Up:
+        y -= cellSize;
+        break;
+      case Direction.Down:
+        y += cellSize;
+        break;
+      default:
+        return null;
+    }
+
+    if (x < 0) {
+      x = canvasSize - cellSize;
+    }
+
+    if (y < 0) {
+      y = canvasSize - cellSize;
+    }
+
+    if (x > canvasSize - cellSize) {
+      x = 0;
+    }
+
+    if (y > canvasSize - cellSize) {
+      y = 0;
+    }
+
+    return { x, y };
+  }
+
+  return null;
+};
 
 class Snake extends PureComponent<ISnakeProps, ISnakeState> {
 
   canvas = React.createRef() as React.RefObject<HTMLCanvasElement>;
-  frameId: any;
+  timerId: any;
 
   state = {
     direction: Direction.Right,
-    snakeSize: 5,
+    snakeSize: INITIAL_SNAKE_SIZE,
     snakeCoordinates: [],
     foodCoordinates: {
       x: -100,
       y: -100,
     },
+    score: 0,
   };
 
   public componentDidMount() {
+    this.focusCanvas();
+    this.setCanvasSize();
     this.startGame();
-    this.loop();
   }
 
   public focusCanvas() {
@@ -60,6 +137,12 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
       canvas.current.focus();
     }
   }
+
+  /**
+   * @method getCanvasContext
+   * @returns {CanvasRenderingContext2D | null}
+   * @description Gets canvas context.
+   */
 
   public getCanvasContext(): CanvasRenderingContext2D | null {
     const { canvas } = this;
@@ -71,7 +154,12 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     return null;
   };
 
-  public setCanvasSize() {
+  /**
+   * @method setCanvasSize
+   * @description Sets canvas size from cell size (props) and field length (constants).
+   */
+
+  public setCanvasSize(): void {
     const { canvas } = this;
     const { cellSize } = this.props;
 
@@ -81,7 +169,13 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     }
   };
 
-  public getCanvasSize() {
+  /**
+   * @method getCanvasSize
+   * @returns {number | null}
+   * @description Gets canvas size (width = height).
+   */
+
+  public getCanvasSize(): number | null {
     const { canvas } = this;
 
     if (canvas && canvas.current) {
@@ -93,7 +187,15 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     return null;
   }
 
-  public renderSquare(x: number, y: number, color: string) {
+  /**
+   * @method renderSquare
+   * @param {number} x
+   * @param {number} y
+   * @param {string} color
+   * @description Gets canvas context and renders rectangle.
+   */
+
+  public renderSquare(x: number, y: number, color: string): void {
     const ctx = this.getCanvasContext();
     const { cellSize } = this.props;
 
@@ -103,7 +205,12 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     }
   }
 
-  public renderGrid() {
+  /**
+   * @method renderGrid
+   * @description Gets canvas context, size and renders grid with cell size step.
+   */
+
+  public renderGrid(): void {
     const ctx = this.getCanvasContext();
     const canvasSize = this.getCanvasSize();
     const { cellSize } = this.props;
@@ -127,8 +234,14 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     }
   }
 
-  public setInitialSnake() {
-    const { snakeSize } = this.state;
+  /**
+   * @method setInitialSnake
+   * @description Set initial snake position (tail on {x: 0, y: 0}), size (constants), direction and score.
+   * Renders snake.
+   */
+
+  public setInitialSnake(): void {
+    const snakeSize = INITIAL_SNAKE_SIZE;
     const { cellSize } = this.props;
     const snakeCoordinates = [];
 
@@ -138,10 +251,15 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
       snakeCoordinates.push({ x: part, y: 0 })
     }
 
-    this.setState({ snakeCoordinates }, () => this.renderSnake());
+    this.setState({ snakeCoordinates, snakeSize, direction: Direction.Right, score: 0 }, () => this.renderSnake());
   }
 
-  public renderSnake() {
+  /**
+   * @method renderSnake
+   * @description Gets snake coordinates from state and renders it to canvas. Re-renders grid and food.
+   */
+
+  public renderSnake(): void {
     const { snakeCoordinates } = this.state;
 
     this.renderGrid();
@@ -149,65 +267,61 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     snakeCoordinates.forEach((coordinate: ICell) => this.renderSquare(coordinate.x, coordinate.y, WHITE_COLOR));
   }
 
-  public moveSnake = () => {
-    const { cellSize } = this.props;
-    const { direction, foodCoordinates, snakeCoordinates } = this.state;
-    let snakeSize = this.state.snakeSize;
-    let { x, y } = snakeCoordinates[0] as ICell;
+  /**
+   * @method moveSnake
+   * @description Computes new head coordinate on the tick and changes snake size if head is on the food coordinate.
+   * Renders snake.
+   */
+
+  public moveSnake = (): void => {
+    let { direction, foodCoordinates, snakeCoordinates, snakeSize, score } = this.state;
     let newSnakeCoordinates = [];
+    const { cellSize } = this.props;
+    const canvasSize = this.getCanvasSize() as number;
+    const headCoordinates = snakeCoordinates[0] as ICell;
+    const newHeadCoordinates = getComputedNextHeadCoordinates(headCoordinates, direction, canvasSize, cellSize);
 
-    switch (direction) {
-      case 'right':
-        x += cellSize;
-        break;
-      case 'left':
-        x -= cellSize;
-        break;
-      case 'up':
-        y -= cellSize;
-        break;
-      case 'down':
-        y += cellSize;
-        break;
-      default:
-        return;
+    if (newHeadCoordinates) {
+
+      if (isSameCoordinates(newHeadCoordinates, foodCoordinates)) {
+        snakeSize++;
+        score++;
+        newSnakeCoordinates = [ newHeadCoordinates, ...snakeCoordinates ];
+        this.setFood();
+      } else {
+        newSnakeCoordinates = [ newHeadCoordinates, ...snakeCoordinates.slice(0, -1) ]
+      }
+
+      this.setState({
+        snakeCoordinates: newSnakeCoordinates,
+        direction,
+        snakeSize,
+        score,
+      }, () => this.renderSnake());
     }
-
-    if (isSameCoordinates({ x, y }, foodCoordinates)) {
-      snakeSize++;
-      newSnakeCoordinates = [ { x, y }, ...snakeCoordinates ];
-      this.setFood();
-    } else {
-      newSnakeCoordinates = [ { x, y }, ...snakeCoordinates.slice(0, -1) ]
-    }
-
-    this.setState({
-      snakeCoordinates: newSnakeCoordinates,
-      direction,
-      snakeSize
-    }, () => this.renderSnake());
   };
+
+  /**
+   * @method handleKeyDown
+   * @description Changes direction if event keyCode matches with arrows keyCodes.
+   */
 
   public handleKeyDown = (e: any) => {
     const { direction } = this.state;
     let newDirection = null;
 
-    if (this.frameId) {
-      clearTimeout(this.frameId);
-    }
-
     switch (e.keyCode) {
       case 37:
-        if (direction !== 'left' && direction !== 'right') newDirection = Direction.Left;
+        if (direction !== Direction.Left && direction !== Direction.Right) newDirection = Direction.Left;
         break;
       case 38:
-        if (direction !== 'up' && direction !== 'down') newDirection = Direction.Up;
+        if (direction !== Direction.Up && direction !== Direction.Down) newDirection = Direction.Up;
         break;
       case 39:
-        if (direction !== 'left' && direction !== 'right') newDirection = Direction.Right;
+        if (direction !== Direction.Left && direction !== Direction.Right) newDirection = Direction.Right;
         break;
       case 40:
-        if (direction !== 'up' && direction !== 'down') newDirection = Direction.Down;
+        if (direction !== Direction.Up && direction !== Direction.Down) newDirection = Direction.Down;
         break;
       default:
         return;
@@ -216,11 +330,14 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     if (newDirection) {
       this.setState({
         direction: newDirection,
-      }, () => this.moveSnake());
+      });
     }
-
-    this.loop();
   };
+
+  /**
+   * @method setFood
+   * @description Sets food random coordinate. Checks collision with snake.
+   */
 
   public setFood() {
     const { cellSize } = this.props;
@@ -237,6 +354,11 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     }
   }
 
+  /**
+   * @method renderFood
+   * @description Gets food coordinate from state. Renders food.
+   */
+
   public renderFood() {
     const foodCoordinates = this.state.foodCoordinates;
 
@@ -247,17 +369,32 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
     }
   }
 
+  /**
+   * @method loop
+   * @description Main game loop. Checks collision when snake's head hits the body.
+   */
+
   public loop = () => {
-    this.frameId = setTimeout(() => {
-      this.moveSnake();
-      this.renderFood();
-      window.requestAnimationFrame(this.loop);
+    this.timerId = setTimeout(() => {
+      const { snakeCoordinates } = this.state;
+
+      if (snakeCoordinates.slice(1).some(item => isSameCoordinates(item, snakeCoordinates[0]))) {
+        clearTimeout(this.timerId);
+        this.startGame();
+
+      } else {
+        this.moveSnake();
+        window.requestAnimationFrame(this.loop);
+      }
     }, 100);
   };
 
+  /**
+   * @method startGame
+   * @description Starts game, renders grid, snake and food.
+   */
+
   public startGame() {
-    this.focusCanvas();
-    this.setCanvasSize();
     this.renderGrid();
     this.setInitialSnake();
     this.setFood();
@@ -265,9 +402,14 @@ class Snake extends PureComponent<ISnakeProps, ISnakeState> {
   }
 
   public render() {
+    const { score } = this.state;
+
     return (
       <div className="wrapper">
         <canvas onKeyDown={this.handleKeyDown} className="canvas" ref={this.canvas} tabIndex={0} />
+        <div className="scoreboard">
+          <span>Score: {score}</span>
+        </div>
       </div>
     );
   }
